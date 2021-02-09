f6fa9351e6c8e2107c1951fb373cd4288efb427d
soukainamabchour <65963285+soukainamabchour@users.noreply.github.com>
2021-02-09 23:01:06 +0100

    Add files via upload

---
+++ WeatherPrediction.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+import warnings
+from sklearn import pipeline,preprocessing,metrics,model_selection,ensemble
+from sklearn_pandas import DataFrameMapper
